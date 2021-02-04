1869150e2feead1da05e6b9eef38238327a6cda7
Armin <arminhajdar1421@gmail.com>
2021-02-04 11:05:08 -0500

    Added delete request to delete by id

--- 2.3/Express/app2/routes/users.js
+++ 2.3/Express/app2/routes/users.js
@@ -3,21 +3,23 @@ const router = express.Router();
 
 const User = require("../models/User"); // With mongoose, you access the collection through the model.
 
-/* GET home page. */
+// GET all users
 router.get("/", function (req, res, next) {
   User.find({}, (err, data) => {
-    if (err) throw err;
+    if (err) console.log(err);
     res.json(data);
   });
 });
 
+// GET user by id
 router.get("/:id", (req, res) => {
   User.findById(req.params.id, (err, data) => {
-    if (err) throw err;
+    if (err) console.log(err);
     res.json(data);
   });
 });
 
+// POST add new user
 router.post("/", (req, res) => {
   let user = new User({
     name: req.body.name,
@@ -25,11 +27,19 @@ router.post("/", (req, res) => {
   });
 
   user.save((err, data) => {
-    if (err) throw err;
+    if (err) throw console.log(err);
     console.log(data);
   });
 
   res.send(user);
+});
+
+// DELETE user by id
+router.delete("/:id", (req, res) => {
+  User.deleteOne({ _id: req.params.id }, (err, data) => {
+    if (err) console.log(err);
+    res.json(data);
+  });
 });
 
 module.exports = router;
