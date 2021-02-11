7d8f4063252956960d2dcf9a734fb48ed5814c49
Armin <arminhajdar1421@gmail.com>
2021-02-11 16:40:02 -0500

    Related Students to Course

--- 2.10/StudentApp/routes/courses.js
+++ 2.10/StudentApp/routes/courses.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const Course = require("../models/Course");
+const Course = require("../models/Course").model;
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
--- 2.10/StudentApp/routes/students.js
+++ 2.10/StudentApp/routes/students.js
@@ -3,9 +3,23 @@ const router = express.Router();
 
 const Student = require("../models/Student");
 
-/* GET users listing. */
+/* GET students listing. */
 router.get("/", function (req, res, next) {
   Student.find({}, (err, data) => {
+    if (err) console.log(err);
+    res.json(data);
+  });
+});
+
+/* POST add student */
+router.post("/", (req, res) => {
+  var student = new Student({
+    name: req.body.name,
+    age: req.body.age,
+    courses: req.body.courses,
+  });
+
+  student.save((err, data) => {
     if (err) console.log(err);
     res.json(data);
   });
