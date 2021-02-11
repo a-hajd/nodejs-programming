8d49fa2c961dad88b43f1cd54e21f795acc145e7
Armin <arminhajdar1421@gmail.com>
2021-02-11 11:15:31 -0500

    Imported routers and connected to database

--- 2.10/StudentApp/app.js
+++ 2.10/StudentApp/app.js
@@ -1,20 +1,23 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const mongoose = require("mongoose");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const studentRouter = require("./routes/students");
 
-var app = express();
+const app = express();
+const url = "mongodb://localhost:27017/StudentAPI";
+mongoose
+  .connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+  .catch((err) => console.log(err));
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/students", studentRouter);
 
 module.exports = app;
