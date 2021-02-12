3e16ab993b908a877d22ca60e0711de9e09dc260
Armin <arminhajdar1421@gmail.com>
2021-02-11 23:51:28 -0500

    Added update and delete route

--- 2.10/StudentApp/routes/students.js
+++ 2.10/StudentApp/routes/students.js
@@ -25,4 +25,20 @@ router.post("/", (req, res) => {
   });
 });
 
+/* PATCH update student */
+router.patch("/:id", (req, res) => {
+  Student.updateOne({ _id: req.params.id }, { $set: req.body }, (err, data) => {
+    if (err) console.log(err);
+    res.json(data);
+  });
+});
+
+/* DELETE student from database */
+router.delete("/:id", (req, res) => {
+  Student.remove({ _id: req.params.id }, (err, data) => {
+    if (err) console.log(err);
+    res.json(data);
+  });
+});
+
 module.exports = router;
