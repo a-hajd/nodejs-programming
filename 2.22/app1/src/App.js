381052eb129b4c3572e624bb862f57f386c48206
Armin <arminhajdar1421@gmail.com>
2021-02-25 17:34:56 -0500

    Reordered app and added routing

--- 2.22/app1/src/App.js
+++ 2.22/app1/src/App.js
@@ -1,40 +1,63 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import React, { Component } from "react";
-import Greeting from "./components/Greeting";
-import Form from "./components/Form";
+import React, { Component, useState } from "react";
+import Landing from "./components/LandingPage/Landing";
+import Login from "./components/Login/Login";
+import Navigation from "./components/Navigation";
+import Dashboard from "./components/Dashboard/Dashboard";
+import Preferences from "./components/Preferences/Preferences";
 
-import Navbar from "react-bootstrap/Navbar";
-import { Nav, Jumbotron } from "react-bootstrap";
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Switch,
+  Route,
+  Link,
+} from "react-router-dom";
 
 class App extends Component {
   state = {};
+
+  setToken = (value) => {
+    this.setState({ token: value });
+  };
+
+  render() {
+    if (!this.state.token) {
+      return <Login setToken={this.setToken} />;
+    }
+
+    return (
+      <div className="wrapper">
+        <Navigation />
+        <Router>
+          <Switch>
+            <Route path="/dashboard">
+              <Dashboard />
+            </Route>
+            <Route path="/preferences">
+              <Preferences />
+            </Route>
+            <Route path="/login">
+              <Login
+                authenticated={this.state.authenticated}
+                redirect_page={"/"}
+                onSubmit={this.handleAuthentication}
+              />
+            </Route>
+            <Route path="/">
+              <Landing />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
 
   handleSubmit = (fields) => {
     console.log(fields);
   };
 
-  render() {
-    return (
-      <div>
-        <Navbar bg="dark" variant="dark" expand="lg">
-          <Navbar.Brand>Brand</Navbar.Brand>
-          <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-          </Nav>
-        </Navbar>
-
-        <Jumbotron>
-          <h1>This is your landing page.</h1>
-          <h4>
-            Hello, <Greeting />
-          </h4>
-        </Jumbotron>
-
-        <Form onSubmit={this.handleSubmit} />
-      </div>
-    );
-  }
+  handleAuthentication = (fields) => {};
 }
 
 export default App;
